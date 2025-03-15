3285214c5e24a9b2a2bc2651321e3998f28f38ab
asfu222 <196063210+asfu222@users.noreply.github.com>
2025-03-14 22:28:24 -0400

    auto update Excel.zip [skip ci]

--- update.sh
+++ update.sh
@@ -33,8 +33,13 @@ download_file() {
 download_file "${BASE_URL}/TableBundles/TableCatalog.bytes" "TableCatalog.bytes"
 download_file "${BASE_URL}/Android/bundleDownloadInfo.json" "bundleDownloadInfo-Android.json"
 download_file "${BASE_URL}/iOS/bundleDownloadInfo.json" "bundleDownloadInfo-iOS.json"
+download_file "${BASE_URL}/TableBundles/Excel.zip" "Excel.zip"
 
 $PYTHON_CMD -m pip install -r requirements.txt
+
+git clone --depth 1 https://github.com/asfu222/BlueArchiveLocalizationTools.git
+find ./assets -type d -path '*/buildSrc/Excel' | xargs -I {} $PYTHON_CMD BlueArchiveLocalizationTools/build_excel_zip.py ./Excel.zip $(dirname {} | sed 's|/buildSrc/Excel||')
+find . -type d -name "*/buildSrc/Excel" -exec rm -rf {} +
 
 chmod +x ./crcmanip-cli
 $PYTHON_CMD patch_table_catalog.py
