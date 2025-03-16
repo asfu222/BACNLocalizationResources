213600a34d58c3e046a3f6cf1320bf996c575fba
asfu222 <196063210+asfu222@users.noreply.github.com>
2025-03-16 13:23:57 -0400

    缓存Excel

---
+++ build.sh
@@ -0,0 +1,29 @@
+if command -v python3 &>/dev/null; then
+    PYTHON_CMD="python3"
+elif command -v python &>/dev/null; then
+    PYTHON_CMD="python"
+else
+    echo "Error: Python is not installed!" >&2
+    exit 1
+fi
+
+echo "Using $PYTHON_CMD"
+
+cd BlueArchiveLocalizationTools
+$PYTHON_CMD -m pip install -r requirements.txt
+find ../assets -type d -path '*/buildSrc/Excel' | while read -r d; do
+  out=$(echo "$d" | sed 's|/buildSrc/Excel/*$|/Excel.zip|')
+  echo ../Excel.zip "$d" "$out"
+  $PYTHON_CMD build_excel_zip.py ../Excel.zip "$d" "$out"
+done
+cd ..
+find . -type d -path "*/buildSrc/Excel" -exec rm -rf {} +
+
+chmod +x ./crcmanip-cli
+$PYTHON_CMD patch_table_catalog.py
+$PYTHON_CMD patch_bundleDL.py
+
+# Clean up
+find . -type d -name "temp" -exec rm -rf {} +
+
+$PYTHON_CMD generate_catalog.py
--- update.sh
+++ update.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-if command -v python3 &>/dev/null; then
-    PYTHON_CMD="python3"
-elif command -v python &>/dev/null; then
-    PYTHON_CMD="python"
-else
-    echo "Error: Python is not installed!" >&2
-    exit 1
-fi
-
-echo "Using $PYTHON_CMD"
-
 export $(grep -v '^#' ba.env | xargs)
 
 BASE_URL=$(curl -s "$BA_SERVER_URL" | grep -oP '"AddressablesCatalogUrlRoot": *"\K[^"]+' | tail -n 1)
@@ -34,22 +23,3 @@ download_file "${BASE_URL}/TableBundles/TableCatalog.bytes" "TableCatalog.bytes"
 download_file "${BASE_URL}/Android/bundleDownloadInfo.json" "bundleDownloadInfo-Android.json"
 download_file "${BASE_URL}/iOS/bundleDownloadInfo.json" "bundleDownloadInfo-iOS.json"
 download_file "${BASE_URL}/TableBundles/Excel.zip" "Excel.zip"
-
-cd BlueArchiveLocalizationTools
-$PYTHON_CMD -m pip install -r requirements.txt
-find ../assets -type d -path '*/buildSrc/Excel' | while read -r d; do
-  out=$(echo "$d" | sed 's|/buildSrc/Excel/*$|/Excel.zip|')
-  echo ../Excel.zip "$d" "$out"
-  $PYTHON_CMD build_excel_zip.py ../Excel.zip "$d" "$out"
-done
-cd ..
-find . -type d -path "*/buildSrc/Excel" -exec rm -rf {} +
-
-chmod +x ./crcmanip-cli
-$PYTHON_CMD patch_table_catalog.py
-$PYTHON_CMD patch_bundleDL.py
-
-# Clean up
-find . -type d -name "temp" -exec rm -rf {} +
-
-$PYTHON_CMD generate_catalog.py
