8b166fc0f54e404f945f81fe13cf84237a4e7dca
asfu222 <196063210+asfu222@users.noreply.github.com>
2025-03-29 23:11:48 -0400

    Revert "split voice zips"
    This reverts commit 2fb07dbd662b471b738dd9ed75972e220b347f03.

--- patch_media_catalog.py
+++ patch_media_catalog.py
@@ -10,23 +10,18 @@ with open("./MediaCatalog.json", "r", encoding="utf8") as f:
     catalog_data = json.loads(f.read())
 for asset_dir in Path('./assets').iterdir():
     media_dir = asset_dir / "latest" / "MediaResources"
-    hasModded = False
-    for i in range(1, 1000):
-        voicepack_path = media_dir / "GameData" / "Audio" / "VOC_CN" / f"CN_Main_{i}.zip"
-        if voicepack_path.is_file():
-            catalog_data["MediaResources"][f"audio/voc_cn/cn_main/cn_main_{i}"] = {
-                "path": f"GameData\\Audio\\VOC_CN\\CN_Main_{i}.zip",
-                "file_name": f"CN_Main_{i}.zip",
-                "bytes": voicepack_path.stat().st_size,
-                "crc": calculate_crc32(voicepack_path),
-                "is_prologue": False,
-                "is_split_download": False,
-                "media_type": 1
-            }
-            hasModded = True
-        else:
-            break
-    if hasModded:
+    voicepack_path = media_dir / "GameData" / "Audio" / "VOC_CN" / "CN_Main.zip"
+
+    if voicepack_path.is_file():
+        catalog_data["MediaResources"]["audio/voc_cn/cn_main/cn_main"] = {
+            "path": "GameData\\Audio\\VOC_CN\\CN_Main.zip",
+            "file_name": "CN_Main.zip",
+            "bytes": voicepack_path.stat().st_size,
+            "crc": calculate_crc32(voicepack_path),
+            "is_prologue": False,
+            "is_split_download": False,
+            "media_type": 1
+        }
         catalog_path = media_dir / "Catalog"
         catalog_path.mkdir(parents=True, exist_ok=True)
         catalog_json_path = catalog_path / "MediaCatalog.json"
